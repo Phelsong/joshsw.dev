32ea01c1d64e77d642103c1b90ec7824a96a5e75
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-05-13 16:50:06 -0500

    formatting

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,31 +1,37 @@
 //----------------------------------------------------------------
 // #Icons
-import { FaPython, FaReact, FaUikit, FaBootstrap, FaNode, FaHtml5, FaSass, FaWindows} from "react-icons/fa";
+import { FaPython, FaReact, FaUikit, FaBootstrap, FaNode, FaSass, FaWindows} from "react-icons/fa";
 import { IoLogoJavascript} from "react-icons/io";
-import { SiPowershell, SiFlask, SiPostgresql, SiCss3, SiDjango} from "react-icons/si";
+import { SiPowershell, SiFlask, SiPostgresql, SiDjango, SiPostman, SiExpress} from "react-icons/si";
 //----------------------------------------------------------------
 
 function About() {
     return (
       <div className="about-container"> 
       <div className="languages-container">
-        languages :
+        Languages :
         <IoLogoJavascript className="about-icon"/>
         <FaPython className="about-icon"/> 
-        <FaHtml5 className="about-icon"/>
-        <SiCss3 className="about-icon"/>
-        <FaSass className="about-icon"/>
-        <SiPostgresql className="about-icon"/>
-        <SiPowershell className="about-icon"/>
         </div>
-        <div className="frameworks-container">
-              frameworks :
-              <FaReact className="about-icon" />
-              <FaNode className="about-icon"/>
+        <div className="frontend-container">
+          Frontend :
+            <FaReact className="about-icon" />
+            <FaSass className="about-icon"/>
+            <FaUikit className="about-icon"/>
+            <FaBootstrap className="about-icon"/>
+          </div>
+        <div className="api-container">
+          Api :
+              <SiExpress className="about-icon"/>
               <SiFlask className="about-icon"/>
               <SiDjango className="about-icon"/>
-              <FaUikit className="about-icon"/>
-              <FaBootstrap className="about-icon"/>
+              <SiPostman className="about-icon"/>
+          </div>
+        <div className="backend-container">
+              Backend :
+              <SiPostgresql className="about-icon"/>
+              <FaNode className="about-icon"/>
+              <SiPowershell className="about-icon"/>
               <FaWindows className="about-icon"/>
           </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from "react-router-dom";
-import './Style/index.css';
+import './Style/index.css'
 import App from './App';
 //----------------------------------------------------------------
 import UIkit from 'uikit';
