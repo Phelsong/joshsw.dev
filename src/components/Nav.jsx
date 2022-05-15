d5695aa3f5d7058e3f457456f1cdf1e2381ba895
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-05-15 17:50:30 -0500

    added project links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,15 @@
 import {Link} from 'react-router-dom'
 
-
+//----------------------------------------------------------------
 function Nav() {
     return (
       <div className="nav-container"> 
-      <Link to="/">About</Link>
-      <Link to="/about">About</Link>
-      <Link to="/Projects">Projects</Link>
+      <Link to="/about" uk-icon="info"></Link>
+      <img src="../Assets/JwilkDevLogo.svg" alt="logo"/>
+      <h3>Josh S Wilkinson</h3>
+      <Link to="/Projects" uk-icon="code"></Link>
       </div>
     );
   }
-  
+//----------------------------------------------------------------
   export default Nav;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,34 @@
-
+import {Link} from 'react-router-dom'
 //----------------------------------------------------------------
 
 function Projects() {
     return (
       <div className="projects-container"> 
-        <img src="../Assets/bluebox splash.jpg" alt="bluebox" className="projects-img"/>
+      <div className="projects-sub-container">
+        <img src="../Assets/blueboxsplash.jpg" alt="bluebox" className="projects-img"/>
+        <span className="projects-name">BlueBox</span>
+        <span className="projects-description">
+            Role: 
+            <br/>
+            Fullstack Developer
+            <br/>
+            I built out the cart...
+         </span>
+         <Link to={`//bluebox-atnight.herokuapp.com/`} target="_blank" uk-icon="link" />
+         <Link to={`//github.com/pjjwGraceShopper/GraceShopper`} target="_blank" uk-icon="github-alt"/>
+      </div>
+      <div className="projects-sub-container">
+      <span className="projects-name">project_ynix</span>
+        <span className="projects-description">
+            Role: 
+            <br/>
+            Fullstack Developer
+            <br/>
+            in Development
+         </span>
+         <Link to="//github.com/Phelsong/project-ynix" target="_blank" uk-icon="github-alt"/>
+          </div>
+
       </div>
     );
   }
