67a7e85066c9eddc09d5ea338c05486a6de9fe93
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-05-15 22:01:31 -0500

    progress

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,17 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 //----------------------------------------------------------------
 function Nav() {
-    return (
-      <div className="nav-container"> 
+  return (
+    <div className="nav-container">
       <Link to="/about" uk-icon="info"></Link>
-      <img src="../Assets/JwilkDevLogo.svg" alt="logo"/>
-      <h3>Josh S Wilkinson</h3>
-      <Link to="/Projects" uk-icon="code"></Link>
+      <div className="nav-logo-container">
+        <img src="../Assets/JwilkDevLogo.svg" alt="logo" />
+        <h3>Josh S Wilkinson</h3>
       </div>
-    );
-  }
+      <Link to="/Projects" uk-icon="code"></Link>
+    </div>
+  );
+}
 //----------------------------------------------------------------
-  export default Nav;
+export default Nav;
