777ff8abf4264120b0a20d06f3105dad1e38e50e
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-06-27 17:26:29 -0500

    nav bar lite work

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 function Nav() {
   return (
     <div className="nav-container">
-      <Link to="/about" uk-icon="info"></Link>
+      <Link to="/about" uk-icon="info" className="nav-icon"></Link>
       <div className="nav-logo-container">
         <svg src={require("../Assets/JwilkDevLogo.svg")} alt="logo"/>
         <h3>Josh S Wilkinson</h3>
       </div>
-      <Link to="/Projects" uk-icon="code"></Link>
+      <Link to="/Projects" uk-icon="code" className="nav-icon"></Link>
     </div>
   );
 }
