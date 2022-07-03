f5aa434dcd8bf18a959416b055c915bf8eea34a9
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-07-03 18:49:48 -0500

    progress on about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,28 +15,28 @@ import {
   SiPowershell,
   SiFlask,
   SiPostgresql,
-  SiDjango,
   SiPostman,
   SiExpress,
 } from "react-icons/si";
-import "../Assets/BeekeeperLogopng.png"
+import "../Assets/BeekeeperLogopng.png";
 //----------------------------------------------------------------
 
 function About() {
   return (
     <div className="about-container">
-      <div className="about-paragraph">
-        I'm formally a working Director of Photography, that has since moved onto development. I'm a University of Texas/FullStack Academy graduate. I love building beautiful solutions to complex problems and creating things that have never been seen before.
+      <div className="bio">
+      <img src={require("../Assets/headshot-small.jpg")} alt="heashot" className="headshot" />
+      <p className="about-paragraph">
+        I spent of the 10years of my career journey as a Directory of Photography, but moved into the Development world in 2022. I love building beautiful solutions to complex problems and creating things that have never been seen before. I'm inspired by automation and anything new and interesting. I aspire to build projects that are well used and loved. Also, eventually working on virtual cinematography projects.
+      </p>
       </div>
       <div className="languages-container">
-      <h5 className="about-title"> Main Languages : </h5>
-      <span className="tech-sub-container">
-        <IoLogoJavascript className="about-icon-main" />
-        JavaScript
-        </span>
-        <span className="tech-sub-container">
-        <FaPython className="about-icon-main" />
-        Python
+        <h5 className="about-title"> Main Languages : </h5>
+        <span className="tech-sub-container tech-about-main">
+          <IoLogoJavascript className="about-icon-main" />
+          JavaScript
+          <FaPython className="about-icon-main" />
+          Python
         </span>
       </div>
       {/* languages ends ^^^^^^^^^^^^^ */}
@@ -44,64 +44,69 @@ function About() {
         <div className="frontend-container">
           <h5 className="about-title"> Frontend : </h5>
           <span className="tech-sub-container">
-          <FaReact className="about-icon" />
-          React
+            <FaReact className="about-icon" />
+            React
           </span>
           <span className="tech-sub-container">
-          <FaSass className="about-icon" />
-          Sass
+            <FaSass className="about-icon" />
+            Sass
           </span>
           <span className="tech-sub-container">
-          <FaUikit className="about-icon" />
-          UiKit
+            <FaUikit className="about-icon" />
+            UiKit
           </span>
           <span className="tech-sub-container">
-          <FaBootstrap className="about-icon" />
-          Bootstrap
+            <FaBootstrap className="about-icon" />
+            Bootstrap
           </span>
         </div>
         {/* FrontEnd Ends ^^^^^^^^^^^^^^^^^^^^ */}
+        {/* API/Backend Starts */}
         <div className="api-container">
-          <h5 className="about-title"> Api : </h5>
+          <h5 className="about-title"> Api & Backend: </h5>
           <span className="tech-sub-container">
-          <SiExpress className="about-icon" />
-          Express
-          </span> 
+            <FaNode className="about-icon" />
+            Node
+          </span>
+
           <span className="tech-sub-container">
-          <SiFlask className="about-icon" />
-          Flask
+            <SiFlask className="about-icon" />
+            Flask
           </span>
           <span className="tech-sub-container">
-          <img src={require("../Assets/fastapi-png.png")} alt="fast api" className="about-icon-manual" />
-          FastAPI
+            <img
+              src={require("../Assets/fastapi-png.png")}
+              alt="fast api"
+              className="about-icon-manual"
+            />
+            FastAPI
           </span>
           <span className="tech-sub-container">
-          <SiPostman className="about-icon" />
-          Postman
+            <SiPostgresql className="about-icon" />
+            PostgreSql
+          </span>
+          <span className="tech-sub-container">
+            <SiExpress className="about-icon" />
+            Express
+          </span>
+          <span className="tech-sub-container">
+            <SiPowershell className="about-icon" />
+            PowerShell
+          </span>
+          <span className="tech-sub-container">
+            <SiPostman className="about-icon" />
+            Postman
+          </span>
+          <span className="tech-sub-container">
+            <img
+              src={require("../Assets/BeekeeperLogopng-black.png")}
+              alt="beekeeper"
+              className="about-icon-manual"
+            />
+            Beekeeper
           </span>
         </div>
-        {/* API ends ^^^^^^^^^^^^^^^^^^ */}
-        {/* Backend Start ************ */}
-        <div className="backend-container">
-          <h5 className="about-title"> Backend :</h5>
-          <span className="tech-sub-container">
-          <SiPostgresql className="about-icon" />
-          PostgreSql
-          </span>
-          <span className="tech-sub-container">
-          <FaNode className="about-icon" />
-          Node
-          </span>
-          <span className="tech-sub-container">
-          <SiPowershell className="about-icon" />
-          PowerShell
-          </span>
-          <span className="tech-sub-container">
-          <img src={require("../Assets/BeekeeperLogopng-black.png")} alt="beekeeper" className="about-icon-manual"/>
-          Beekeeper 
-            </span>
-        </div>
-        {/* Backend ends ^^^^^^^^^^^^^^ */}
+        {/* API/Backend ends ^^^^^^^^^^^^^^ */}
       </div>
     </div>
   );
